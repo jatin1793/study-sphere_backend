2c695e31c6156606afb7607d81eca009985bd669
jatin1793 <shanavyabhatt@gmail.com>
2023-12-14 19:00:21 +0530

    ✔

--- routes/instructorRoutes.js
+++ routes/instructorRoutes.js
@@ -8,14 +8,14 @@ const { instructor_authenticateJWT } = require('../middleware/authenticateJWT.js
 const upload = require('../middleware/multer.js');
 
 
-// Instructor     -->      /instructor/      
+// Instructor         
 app.post('/register', InstructorRegister);
 app.post('/login', InstructorLogin)
 app.post('/update', UpdateProfile)
 app.post('/logout', instructor_authenticateJWT, InstructorLogout)
 app.post('/details', instructor_authenticateJWT, Instructordetails)
 
-// Courses       -->      /instructor/  
+// Courses         
 app.get('/mycourses', instructor_authenticateJWT, Mycourses)
 app.post('/createcourse',upload.single('file'), instructor_authenticateJWT, createCourse)
 app.get('/allcourses', instructor_authenticateJWT, AllCourses)
@@ -23,7 +23,7 @@ app.delete('/course/deletecourse/:courseid',instructor_authenticateJWT, deleteCo
 app.post('/course/:id',instructor_authenticateJWT, showCoursedetails )
 
 
-// Videos        -->      /instructor/  
+// Videos       
 app.post('/:id/allvideos', instructor_authenticateJWT,AllVideos)
 app.delete('/deletevideo/:id', instructor_authenticateJWT,DeleteVideo)
 app.post('/uploadvideo/:courseid',instructor_authenticateJWT , upload.single('file'), uploadVideo)
